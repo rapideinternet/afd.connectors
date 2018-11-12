3c9f55edd4bfe2b52368f75be0d6c56525ab93fc
Lucas van der Have <lucasvdhave@gmail.com>
2018-11-12 12:34:29 +0100

    :art: Change typehint

--- src/Connectors/Contracts/TIMEConnector.php
+++ src/Connectors/Contracts/TIMEConnector.php
@@ -5,19 +5,20 @@ namespace SIVI\AFDConnectors\Connectors\Contracts;
 use SIVI\AFD\Models\Contracts\Message;
 use SIVI\AFDConnectors\Enums\TIME\MessageStatus;
 use SIVI\AFDConnectors\Exceptions\FetchingWSDLFailedException;
+use SIVI\AFDConnectors\Interfaces\BatchMessage;
 use SIVI\AFDConnectors\Interfaces\Connector;
 
 interface TIMEConnector extends Connector
 {
 
     /**
-     * @return \SIVI\AFDConnectors\Interfaces\TIME\Message[]
+     * @return BatchMessage[]
      */
     public function getMessages();
 
     /**
      * @param MessageStatus $messageStatus
-     * @return Message[]
+     * @return BatchMessage[]
      * @throws FetchingWSDLFailedException
      */
     public function getMessagesByStatus(MessageStatus $messageStatus);
